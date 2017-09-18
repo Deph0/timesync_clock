e7f855264dc62395fa5ae2bf1b4262b53f6a5da6
root <root@ubuntu.defaultdomain>
2017-09-18 06:42:21 +0000

    Added date to clockview

--- public/js/index.js
+++ public/js/index.js
@@ -9,9 +9,12 @@ function displayTime() {
   var date = new Date(ts.now());
   var thm = document.getElementById("timeHourMinute");
   var tis = document.getElementById("timeSeconds");
+  var td = document.getElementById("timeDate");
   thm.innerHTML = date.toLocaleTimeString('sv-SE', {hour: '2-digit', minute:'2-digit'});
   tis.innerHTML = date.toLocaleTimeString('sv-SE').slice(-2);
   //tis.innerHTML = date.toLocaleTimeString('en-US',{second:'2-digit'});
   //tis.innerHTML = addZero(date.getSeconds());
+  var options = { weekday: 'long', year: 'numeric', month: '2-digit', day: '2-digit' }; 
+  td.innerHTML = date.toLocaleDateString('sv-SE', options);
 }
 setInterval(displayTime, 1000);
